1135cda4ab156d7465bbf1053b97f374b0a15e77
Liubomyr <43443223+Lub4ikChe@users.noreply.github.com>
2022-05-10 12:15:52 +0300

    fix: add missing like icon (#16)

--- src/pages/Track/TrackHeader/styles.ts
+++ src/pages/Track/TrackHeader/styles.ts
@@ -1,6 +1,18 @@
 import styled from '@emotion/styled';
+
+import { IconButton } from '@mui/material';
 import HeadphonesRoundedIcon from '@mui/icons-material/HeadphonesRounded';
 
 export const StyledIcon = styled(HeadphonesRoundedIcon)`
   vertical-align: middle;
 `;
+
+export const StyledIconButton = styled(IconButton)`
+  padding: 0;
+  color: #1a76d2;
+`;
+
+export const StyledLikedIconProps = {
+  width: 40,
+  height: 40,
+};
